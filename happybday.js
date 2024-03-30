71b04a968ab2e4ec924ef44eb42cae2d126f9589
Bryan Herdianto <73747143+bryanherdianto@users.noreply.github.com>
2024-03-30 22:31:31 +0700

    edited so perfect

--- happybday.js
+++ happybday.js
@@ -1,31 +1,10 @@
-const text = document.querySelector('.text');
 const container = document.querySelector('.container');
-
-let x = 0;
-let y = 0;
-let xSpeed = 2;
-let ySpeed = 2;
-
-function moveText() {
-    x += xSpeed;
-    y += ySpeed;
-
-    if (x + text.clientWidth > container.clientWidth || x < 0) {
-        xSpeed = -xSpeed;
-    }
-
-    if (y + text.clientHeight > container.clientHeight || y < 0) {
-        ySpeed = -ySpeed;
-    }
-
-    text.style.transform = `translate(${x}px, ${y}px)`;
-    requestAnimationFrame(moveText);
-}
 
 function createFirework() {
     const firework = document.createElement('div');
     firework.classList.add('firework');
     firework.style.left = `${Math.random() * container.clientWidth}px`;
+    firework.style.top = `${Math.random() * container.clientWidth}px`;
     container.appendChild(firework);
 
     // Remove the firework element after the explosion animation
